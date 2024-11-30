3c4324a734d0fa6afc2f0a7a6dbbb5560f1b0ac4
shariful962 <shariful0411@gmail.com>
2024-11-30 19:03:20 +0600

    first commit

---
+++ JS/main.js
@@ -0,0 +1,18 @@
+let nav = document.querySelector(".navbar");
+window.addEventListener('scroll', () => {
+    if (window.scrollY > 80) {
+      nav.classList.add('header-scrolled');
+    } else {
+      nav.classList.remove('header-scrolled');
+    }
+  });
+
+// nav hide
+
+let navBar = document.querySelectorAll(".nav-link");
+let navCollapse = document.querySelector(".navbar-collapse.collapse");
+navBar.forEach(function(a){
+    a.addEventListener("click",function(){
+        navCollapse.classList.remove("show");
+    })
+})
